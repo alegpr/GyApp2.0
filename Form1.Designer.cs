455623f6843c98071de429b5848430d3c5a1f288
M_INF_4G <M_INF_4G@C211-06>
2026-03-03 16:14:38 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GyApp2._0
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(1058, 1864);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(513, 926);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1058, 1864);
+            Controls.Add(tableLayoutPanel1);
+            MaximizeBox = false;
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += frmMain_Load;
+            Resize += frmMain_Resize;
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Windows.Forms;
+
+namespace GyApp2._0
+{
+    public partial class frmMain : Form
+    {
+        private const float aspectRatio = 16f / 9f;   //definisce l'aspect ratio
+        public frmMain()
+        {
+            InitializeComponent();
+            AdattaProporzioni();   //adatta le proporzioni al caricamento iniziale
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            AdattaProporzioni();
+        }
+        // !!! METODI CUSTOM !!!
+        private void AdattaProporzioni()
+        {
+            
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal; //riportalo a dimensione normale
+            }
+
+            int targetHeight = (int)(this.Width * aspectRatio);
+
+            //non permettere che l'altezza superi l'80% dello schermo
+            int maxHeight = (int)(Screen.PrimaryScreen.Bounds.Height * 0.8);
+
+            if (targetHeight > maxHeight)
+            {
+                targetHeight = maxHeight;
+                this.Width = (int)(targetHeight / aspectRatio); //ricalcola la larghezza per non rompere il ratio
+            }
+
+            if (this.Height != targetHeight)
+            {
+                this.Size = new System.Drawing.Size(this.Width, targetHeight);
+            }
+        }
+    }
+}
